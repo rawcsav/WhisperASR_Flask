0909449a64d6b6dd45d0b3028354e75a533431e6
gavin <gavin@rawcsav.com>
2023-08-22 03:31:14 -0400

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,160 +1,155 @@
-import atexit
-import threading
-
-from flask import Flask, render_template_string, request, get_flashed_messages, \
+from flask import Flask, render_template, request, get_flashed_messages, \
     send_from_directory, redirect, url_for, flash
+from processing import TranscriptionFailedException
 from werkzeug.exceptions import RequestEntityTooLarge
-from main import transcribe_files
+from main import transcribe_files, check_api_key
+from apscheduler.schedulers.background import BackgroundScheduler
 import os
+import time
 import shutil
 import tempfile
-import time
+from werkzeug.utils import secure_filename
+import contextlib
 
-# Initiate Flask app
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # Secret key for Flask's flash mechanism
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100MB
-SUPPORTED_FORMATS = [".mp3", ".mp4", ".mpeg", ".mpga", ".m4a", ".wav", ".webm"]  # List of supported audio formats
+app.config['SECRET_KEY'] = os.urandom(24)
+#app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # 50MB
 
-# Global set to track temporary directories
+CLEANUP_THRESHOLD_SECONDS = 3600
+SUPPORTED_FORMATS = (".mp3", ".mp4", ".mpeg", ".mpga", ".m4a", ".wav", ".webm")
+
 temp_dirs = set()
 
-index_html = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transcription Service</title>
-    <!-- The rest of the styles remain the same -->
-</head>
-<body>
-    <div class="navbar">
-        <h1>Transcription Service</h1>
-    </div>
+class FileNotFoundError(Exception):
+    pass
 
-    <div class="container">
-        <form action="/transcribe" method="post" enctype="multipart/form-data">
-            <label for="audio_files">Select individual audio files:</label>
-            <div class="file-input-wrapper">
-                <label for="audio_files" class="file-input-button">Choose Files</label>
-                <input type="file" name="audio_files" id="audio_files" multiple>
-            </div>
-            <label for="api_key">OpenAI API Key:</label>
-            <input type="password" name="api_key" id="api_key" required>
-            <br><br>
-            <button type="submit">Transcribe</button>
-        </form>
-    </div>
-</body>
-</html>
-'''
 
 @app.route('/', methods=['GET'])
 def index():
     messages = get_flashed_messages()
-    return render_template_string(index_html, messages=messages)
+    return render_template("index.html", messages=messages)
 
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
-    # Get the uploaded files and API key from the request
     audio_files = request.files.getlist('audio_files')
-    audio_directory = request.files.getlist('audio_directory')
     api_key = request.form.get('api_key')
+    use_timestamps = request.form.get('use_timestamps') == 'yes'
+    language = request.form.get('language')
+    translate = request.form.get('translate') == 'yes'
 
-    # Flash a message about receiving the files
-    flash("Received files. Checking for supported formats...", "info")
+    # Validate API key
+    if not check_api_key(api_key):
+        flash("Invalid API key! Please check your API key and try again.", "error")
+        return redirect(url_for('index'))
 
-    # Create temporary directory for output and input
-    output_directory = tempfile.mkdtemp()
-    input_directory = tempfile.mkdtemp()
+    if not audio_files or not api_key:
+        flash("Please upload audio files and provide an API key.", "error")
+        return redirect(url_for('index'))
 
-    # Store directory creation timestamp
-    temp_dirs.add((output_directory, time.time()))
-    temp_dirs.add((input_directory, time.time()))
-
-    # Merge the content of the audio files and the audio_directory based on user's choice
-    all_uploads = audio_files if audio_files else audio_directory
-
-    # Filter out files that are not in supported formats
-    valid_files = [uploaded_file for uploaded_file in all_uploads if
-                   os.path.splitext(uploaded_file.filename)[1] in SUPPORTED_FORMATS]
+    valid_files = [file for file in audio_files if file.filename.endswith(SUPPORTED_FORMATS)]
 
     if not valid_files:
         flash("No supported audio files selected!", "error")
         return redirect(url_for('index'))
 
-    # Flash a message about saving the valid files
-    flash("Saving valid audio files for transcription...", "info")
+    # Corrected indentation starts here
+    output_directory = tempfile.mkdtemp(prefix="output_")  # Add prefix to output directory
+    input_directory = tempfile.mkdtemp(prefix="input_")  # Add prefix to input directory
+
+    temp_dirs.add((output_directory, time.time()))
+    temp_dirs.add((input_directory, time.time()))
 
     for uploaded_file in valid_files:
-        file_path = os.path.join(input_directory, uploaded_file.filename)
+        filename = secure_filename(uploaded_file.filename)
+        file_path = os.path.join(input_directory, filename)
         uploaded_file.save(file_path)
 
-    # Provide a status update to the user
-    flash("Transcription started. Please wait...", "info")
+    try:
+        transcribe_files(input_directory, output_directory, api_key, use_timestamps, language, translate)
+        print(
+            f"Transcription completed! Input directory: {input_directory}, Output directory: {output_directory}")  # Add this line
+    except TranscriptionFailedException as e:
+        flash(str(e), "error")
+        return redirect(url_for('index'))
 
-    # Call the transcribe_files function
-    transcribe_files(input_directory, output_directory, api_key)
-    shutil.rmtree(input_directory)
-    # Provide a status update to the user
-    flash("Transcription completed!", "success")
-
-    # Redirect to the results page
     return redirect(url_for('results', output_dir=output_directory))
+
 
 @app.route('/results', methods=['GET'])
 def results():
     output_directory = request.args.get('output_dir')
-    files = os.listdir(output_directory)
-    return render_template_string('''
-    <h2>Transcription Results</h2>
-    <ul>
-    {% for file in files %}
-        <li><a href="{{ url_for('download', filename=file, output_dir=output_directory) }}">{{ file }}</a></li>
-    {% endfor %}
-    </ul>
-    <p><a href="{{ url_for('index') }}">Go Back</a></p>
-    ''', files=files, output_directory=output_directory)
+    print(f"Output directory: {output_directory}")
 
+    if not output_directory:
+        flash("Missing output directory.", "error")
+        return redirect(url_for('index'))
+
+    try:
+        files = os.listdir(output_directory)
+        print(f"Files: {files}")
+    except FileNotFoundError:
+        flash("Output directory not found.", "error")
+        return redirect(url_for('index'))
+
+    return render_template("results.html", files=files, output_directory=output_directory)
 
 @app.route('/download/<filename>', methods=['GET'])
 def download(filename):
-    output_directory = request.args.get('output_dir')
-    response = send_from_directory(output_directory, filename, as_attachment=True)
-    os.remove(os.path.join(output_directory, filename))
+    output_directory = request.args.get('output_dir')  # Correctly retrieving from query parameters
 
-    # Check if directory is empty and remove it if it is
+    if not output_directory:
+        flash("Missing output directory.", "error")
+        return redirect(url_for('index'))
+
+    file_path = os.path.join(output_directory, filename)
+
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {filename}")
+
+    response = send_from_directory(output_directory, filename, as_attachment=True)
+    os.remove(file_path)
+
     if not os.listdir(output_directory):
         os.rmdir(output_directory)
+        temp_dirs.discard((output_directory, time.time()))
 
     return response
 
-@app.errorhandler(RequestEntityTooLarge)
-def file_too_large(e):
-    flash("File is too large! Please upload files less than 100MB.", "error")
+
+#@app.errorhandler(RequestEntityTooLarge)
+#def file_too_large(e):
+    #flash("File is too large! Please upload files less than 50MB.", "error")
+    #return redirect(url_for('index'))
+
+
+@app.errorhandler(TranscriptionFailedException)
+def handle_transcription_failed(e):
+    flash("Transcription failed! Please check your API key and try again.", "error")
     return redirect(url_for('index'))
+
+
+@app.errorhandler(FileNotFoundError)
+def handle_file_not_found(e):
+    flash("File not found! Please check your uploaded files and try again.", "error")
+    return redirect(url_for('index'))
+
 
 def cleanup():
     current_time = time.time()
-    # Define a threshold in seconds for how old a directory should be before it's deleted
-    # For example, 1 hour = 3600 seconds
-    threshold = 3600
-    dirs_to_remove = []
-    for directory, timestamp in temp_dirs:
-        if current_time - timestamp > threshold:
-            shutil.rmtree(directory, ignore_errors=True)
-            dirs_to_remove.append((directory, timestamp))
-    # Remove deleted directories from the temp_dirs set
-    for directory in dirs_to_remove:
-        temp_dirs.remove(directory)
+    dirs_to_remove = {(directory, timestamp) for directory, timestamp in temp_dirs if
+                      current_time - timestamp > CLEANUP_THRESHOLD_SECONDS and (directory.startswith('input_') or directory.startswith('output_'))}
 
-def periodic_cleanup(interval=3600):  # Default to 1 hour
-    while True:
-        time.sleep(interval)
-        cleanup()
+    for directory, _ in dirs_to_remove:
+        shutil.rmtree(directory, ignore_errors=True)
 
-cleanup_thread = threading.Thread(target=periodic_cleanup)
-cleanup_thread.daemon = True  # Daemonize thread to close when main process exits
-cleanup_thread.start()
+    temp_dirs.difference_update(dirs_to_remove)
+
+def start_cleanup_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(cleanup, 'interval', seconds=CLEANUP_THRESHOLD_SECONDS)
+    scheduler.start()
+
+if __name__ == '__main__':
+    start_cleanup_scheduler()
+    app.run(debug=True)
