147e2f57a1f3ad147033cec5924e29581530c410
gavin <gavin@rawcsav.com>
2023-08-22 03:31:02 -0400

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -2,139 +2,164 @@ import openai
 import os
 from pydub import AudioSegment
 import re
+from typing import Optional, Tuple, List
+from pathlib import Path, PurePath
 
 initial_prompt="Hello, welcome to my lecture."
 
-def convert_to_mp3(file_path):
+def convert_to_mp3(file_path: str) -> Optional[str]:
     basename, extension = os.path.splitext(file_path)
+    extension = extension.lower()
     mp3_file_path = f"{basename}.mp3"
 
-    if extension.lower() in ['.mp4', '.mpeg', '.mpga', '.m4a', '.wav', '.webm']:
-        audio = AudioSegment.from_file(file_path, extension.lower().lstrip('.'))
+    supported_extensions = ['.mp4', '.mpeg', '.mpga', '.m4a', '.wav', '.webm']
+    if extension in supported_extensions:
+        audio = AudioSegment.from_file(file_path, extension.lstrip('.'))
         audio.export(mp3_file_path, format="mp3")
         return mp3_file_path
     else:
-        return file_path
-
-
-def transcribe_with_retry(audio_file, prompt):
-    success = False
+        return None
+def transcribe_with_retry(audio_file: str, prompt: str, max_retries: int = 3, use_timestamps: bool = True, language: str = 'en') -> Optional[str]:
     transcript = None
-    max_retries = 3
-    retry_count = 0
 
-    while not success and retry_count < max_retries:
+    for retry_count in range(max_retries):
         try:
-            transcript = openai.Audio.transcribe("whisper-1", audio_file, response_format="srt", language="en",
-                                                 prompt=prompt)
-            success = True
+            if use_timestamps:
+                response = openai.Audio.transcribe("whisper-1", audio_file, response_format="srt", language=language, prompt=prompt)
+                transcript = response
+            else:
+                response = openai.Audio.transcribe("whisper-1", audio_file, language=language, prompt=prompt)
+                transcript = response['text']
+            return transcript
         except Exception as e:
             print(f"Error: {e}. Retrying...")
-            retry_count += 1
 
-    if not success:
-        print("Max retries reached. Failed to transcribe audio.")
+    return None
 
-    return transcript
-def parse_transcript_text(transcript_srt):
-    parsed_subtitles = parse_subtitles(transcript_srt)
-    transcribed_text = ""
-    for subtitle, text in parsed_subtitles:
-        transcribed_text += text + " "
+def translate_with_retry(audio_file: str, prompt: str, max_retries: int = 3, use_timestamps: bool = True) -> Optional[str]:
+    transcript = None
+
+    for retry_count in range(max_retries):
+        try:
+            if use_timestamps:
+                response = openai.Audio.translate("whisper-1", audio_file, response_format="srt", prompt=prompt)
+                translation = response
+            else:
+                response = openai.Audio.translate("whisper-1", audio_file, prompt=prompt)
+                translation = response['text']
+            return translation
+        except Exception as e:
+            print(f"Error: {e}. Retrying...")
+
+    return None
+
+def parse_transcript_text(transcript_srt: str) -> str:
+    parsed_timestamps = parse_timestamps(transcript_srt)
+    transcribed_text = " ".join(text for timestamp, text in parsed_timestamps)
     return transcribed_text.strip()
 
-def process_audio_file(filename, input_directory, openai_api_key):
+
+def process_audio_file(filename: str, input_directory: str, openai_api_key: str, use_timestamps: bool = True,
+                       language: str = 'en', translate: bool = False) -> Optional[str]:
     max_file_size = 25 * 1024 * 1024  # 25 MB
     supported_formats = (".mp3", ".mp4", ".mpeg", ".mpga", ".m4a", ".wav", ".webm")
-    file_path = os.path.join(input_directory, filename)
-    file_size = os.path.getsize(file_path)
+    file_path = Path(input_directory) / filename
+    file_size = file_path.stat().st_size
 
     openai.api_key = openai_api_key
 
     # If the file is a compatible format and is smaller than 25MB
-    if file_size <= max_file_size and filename.endswith(supported_formats):
+    if file_size <= max_file_size and file_path.suffix.lower() in supported_formats:
         with open(file_path, "rb") as audio_file:
-            return transcribe_with_retry(audio_file, prompt=initial_prompt)
+            if translate:
+                transcript = translate_with_retry(audio_file, initial_prompt, use_timestamps=use_timestamps)
+            else:
+                transcript = transcribe_with_retry(audio_file, initial_prompt, language=language, use_timestamps=use_timestamps)
+            return transcript
 
     # If the file is larger than 25MB and is a compatible format but not an MP3
-    elif file_size > max_file_size and filename.endswith(supported_formats) and not filename.endswith(".mp3"):
+    elif file_size > max_file_size and file_path.suffix.lower() in supported_formats and file_path.suffix.lower() != ".mp3":
         mp3_file_path = convert_to_mp3(file_path)
-        os.remove(file_path)
+        file_path.unlink()
 
     # If the file is an MP3 and is larger than 25MB
-    elif file_size > max_file_size and filename.endswith(".mp3"):
+    elif file_size > max_file_size and file_path.suffix.lower() == ".mp3":
         mp3_file_path = file_path
 
     else:
-        return None  # Return None or handle other file types accordingly
+        return None
 
     # Check file size of mp3_file_path and split if necessary
-    file_size = os.path.getsize(mp3_file_path)
+    file_size = mp3_file_path.stat().st_size
     if file_size > max_file_size:
         file_parts = split_large_file(mp3_file_path)
-        os.remove(mp3_file_path)
+        mp3_file_path.unlink()
         transcripts_parts = []
-        prompt=''
+        prompt = ''
 
         for part_idx, part_file in enumerate(file_parts):
             with open(part_file, "rb") as audio_file:
-                transcript_part = transcribe_with_retry(audio_file, prompt=prompt)
+                if translate:
+                    transcript_part = translate_with_retry(audio_file, prompt=prompt, use_timestamps=use_timestamps)
+                else:
+                    transcript_part = transcribe_with_retry(audio_file, prompt=prompt, language=language,
+                                                        use_timestamps=use_timestamps)
                 transcripts_parts.append(transcript_part)
 
             # Reset the prompt to an empty string for all parts except the first one
-            if part_idx == 0:
-                prompt = "Hello, welcome to my lecture."
-            else:
-                prompt = ""
+            prompt = "Hello, welcome to my lecture." if part_idx == 0 else ""
 
             # Update prompt with transcribed text for the next part
             if transcript_part is not None:
-                prompt += parse_transcript_text(transcript_part)
+                if use_timestamps:
+                    prompt += parse_transcript_text(transcript_part)
+                else:
+                    prompt += transcript_part.strip()
 
             # Delete the part after transcribing
-            os.remove(part_file)
+            Path(part_file).unlink()
 
-        # Append transcripts and maintain timestamps
-        # Append transcripts and maintain timestamps
         transcripts = ''
         timestamp_offset = 0
 
-        for part_transcripts in transcripts_parts:
-            parsed_subtitles = parse_subtitles(part_transcripts)
-            last_timestamp = 0
-            last_end_timestamp = 0
+        if use_timestamps:
+            for part_transcripts in transcripts_parts:
+                parsed_timestamps = parse_timestamps(part_transcripts)
+                last_end_timestamp = 0
 
-            for subtitle, text in parsed_subtitles:
-                match = re.search(r'(\d+:\d+:\d+,\d+)\s+-->\s+(\d+:\d+:\d+,\d+)', subtitle)
-                if match:
-                    start_timestamp = match.group(1)
-                    end_timestamp = match.group(2)
-                    start_time = timestamp_to_ms(start_timestamp)
-                    end_time = timestamp_to_ms(end_timestamp)
-                else:
-                    print(f"Failed to find timestamp in subtitle: {subtitle}")
-                    continue
+                for timestamp, text in parsed_timestamps:
+                    match = re.search(r'(\d+:\d+:\d+,\d+)\s+-->\s+(\d+:\d+:\d+,\d+)', timestamp)
+                    if match:
+                        start_timestamp = match.group(1)
+                        end_timestamp = match.group(2)
+                        start_time = timestamp_to_ms(start_timestamp)
+                        end_time = timestamp_to_ms(end_timestamp)
+                    else:
+                        print(f"Failed to find timestamp in timestamp: {timestamp}")
+                        continue
 
-                new_start_time = ms_to_timestamp(start_time + timestamp_offset)
-                new_end_time = ms_to_timestamp(end_time + timestamp_offset)
+                    new_start_time = ms_to_timestamp(start_time + timestamp_offset)
+                    new_end_time = ms_to_timestamp(end_time + timestamp_offset)
 
-                new_subtitle = subtitle.replace(start_timestamp, new_start_time)
-                new_subtitle = new_subtitle.replace(end_timestamp, new_end_time)
+                    new_timestamp = timestamp.replace(start_timestamp, new_start_time)
+                    new_timestamp = new_timestamp.replace(end_timestamp, new_end_time)
 
-                transcripts += new_subtitle + "\n" + text + "\n\n"
+                    transcripts += new_timestamp + "\n" + text + "\n\n"
 
-                last_end_timestamp = end_time
+                    last_end_timestamp = end_time
 
-            timestamp_offset += last_end_timestamp
+                timestamp_offset += last_end_timestamp
+        else:
+            transcripts = " ".join(transcripts_parts)
 
-        return transcripts
+    return transcripts
 
-def parse_subtitles(content):
-    parsed_subtitles = re.findall(r'(\d+\n\d+:\d+:\d+,\d+ --> \d+:\d+:\d+,\d+\n(.*?)(?:\n|\Z))', content, re.DOTALL)
-    if not parsed_subtitles:
-        print(f"Error: Unable to parse subtitles from content: {content}")
-    return parsed_subtitles
 
+def parse_timestamps(content: str) -> List[Tuple[str, str]]:
+    parsed_timestamps = re.findall(r'(\d+\n\d+:\d+:\d+,\d+ --> \d+:\d+:\d+,\d+\n(.*?)(?:\n|\Z))', content, re.DOTALL)
+    if not parsed_timestamps:
+        print(f"Error: Unable to parse timestamps from content: {content}")
+    return parsed_timestamps
 
 def is_full_sentence(text):
     return text.strip() and text[-1] in '.!?'
@@ -148,54 +173,53 @@ def format_timestamp(timestamp):
         return f'{m:02d}:{s:02d}'
 
 
-def process_subtitles(subtitles):
-    new_subtitles = []
-    subtitle_buffer = ''
+def process_timestamps(timestamps):
+    new_timestamps = []
+    timestamp_buffer = ''
     start_time = ''
     end_time = ''
 
-    for subtitle, text in subtitles:
+    for timestamp, text in timestamps:
         if not start_time:
-            start_time = re.search(r'(\d+:\d+:\d+,\d+) -->', subtitle).group(1)
+            start_time = re.search(r'(\d+:\d+:\d+,\d+) -->', timestamp).group(1)
 
-        subtitle_buffer += ' ' + text.strip()
+        timestamp_buffer += ' ' + text.strip()
 
-        if is_full_sentence(subtitle_buffer):
-            end_time = re.search(r'--> (\d+:\d+:\d+,\d+)', subtitle).group(1)
-            new_subtitles.append((start_time, end_time, subtitle_buffer.strip()))
-            subtitle_buffer = ''
+        if is_full_sentence(timestamp_buffer):
+            end_time = re.search(r'--> (\d+:\d+:\d+,\d+)', timestamp).group(1)
+            new_timestamps.append((start_time, end_time, timestamp_buffer.strip()))
+            timestamp_buffer = ''
             start_time = ''
             end_time = ''
 
-    return new_subtitles
+    return new_timestamps
 
 
-def export_subtitles(subtitles, filename):
+def export_timestamps(timestamps: List[Tuple[str, str, str]], filename: str) -> None:
     with open(filename, 'w') as f:
-        for start, end, text in subtitles:
+        for start, end, text in timestamps:
             start = format_timestamp(start)
             end = format_timestamp(end)
             f.write(f'[{start} - {end}] {text}\n\n')
 
 
-def timestamp_to_ms(timestamp):
+def timestamp_to_ms(timestamp: str) -> int:
     hours, minutes, remaining = timestamp.split(':')
     seconds, milliseconds = remaining.split(',')
     total_ms = int((int(hours) * 3600 + int(minutes) * 60 + float(seconds)) * 1000 + int(milliseconds))
     return total_ms
 
-
-def ms_to_timestamp(ms):
+def ms_to_timestamp(ms: int) -> str:
     seconds, milliseconds = divmod(ms, 1000)
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d},{int(milliseconds):03d}"
 
 
-def split_large_file(file_path):
+def split_large_file(file_path: Path) -> List[str]:
     split_duration_ms = 24 * 60 * 1000  # 24 minutes
     audio = AudioSegment.from_file(file_path)
-    file_basename, _ = os.path.splitext(file_path)
+    file_basename = file_path.stem
     file_parts = []
 
     for i in range(0, len(audio), split_duration_ms):
@@ -208,6 +232,9 @@ def split_large_file(file_path):
 
 
 def process_transcripts(content, output_file):
-    parsed_subtitles = parse_subtitles(content)
-    formatted_subtitles = process_subtitles(parsed_subtitles)
-    export_subtitles(formatted_subtitles, output_file)
+    parsed_timestamps = parse_timestamps(content)
+    formatted_timestamps = process_timestamps(parsed_timestamps)
+    export_timestamps(formatted_timestamps, output_file)
+
+class TranscriptionFailedException(Exception):
+    pass
